8f29305cae96daf3ab12426d5c5386bf8e12781f
Anton Suprun <kpobococ@gmail.com>
2009-12-04 01:44:29 +0200

    fixed: AeRequest, todo: add REQUEST global support to AeRequest

--- ae/classes/request.class.php
+++ ae/classes/request.class.php
@@ -1,11 +1,9 @@
 <?php
-
+/**
+ * @todo write documentation
+ */
 class AeRequest extends AeObject
 {
-    /**
-     *
-     * @var string
-     */
     protected $_site = null;
     protected $_directory = null;
     protected $_virtual = null;
@@ -15,9 +13,6 @@ class AeRequest extends AeObject
     protected $_name = null;
     protected $_rewrite = false;
     protected $_extensions = array();
-
-    // TODO: remove after debug finished
-    public $timer;
 
     public function __construct($rewrite = false, $name = null, $extensions = array())
     {
@@ -30,11 +25,9 @@ class AeRequest extends AeObject
 
     public function isClean()
     {
-        $clean = '';
+        $clean = '/' . preg_replace('#^' . $this->_site . '#', '', $this->toString());
 
-        if ($this->_directory !== false) {
-            $clean = '/' . $this->_directory;
-        }
+        return $clean === $_SERVER['REQUEST_URI'];
     }
 
     public function toString()
@@ -66,19 +59,23 @@ class AeRequest extends AeObject
 
     protected function _parseRequest()
     {
-        // TODO: remove after debug finished
-        $this->timer = new AeTimer();
-
+        // *** Already parsed?
         if ($this->_site !== null) {
             return $this;
         }
 
+        // *** Check for command line mode
         if (!isset($_SERVER['HTTP_HOST'])) {
             $this->_site = false;
             throw new AeRequestException('Request parser does not support command line mode', 405);
         }
 
         // *** Protocol, host and port
+        /*
+         * Apparently, this detection will fail on lighttpd, and there is no way
+         * to tell whether a https connection was used for a request on that
+         * server, so I'm all out of ideas.
+         */
         $protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https' : 'http';
 
         if ($protocol == 'https') {
@@ -93,16 +90,29 @@ class AeRequest extends AeObject
         $script_name = substr($_SERVER['SCRIPT_NAME'], 1);
         $bits        = explode('/', $script_name);
 
-        // TODO: fix issue with index.php, when rewrite is set to false but is actually used
-        if ($this->_rewrite === true)
+        foreach ($bits as $i => $bit)
         {
-            // *** Remove script file name from URL
-            if (strpos($script_name, '/') !== false) {
-                array_pop($bits);
-            } else {
-                $bits = array();
+            if (empty($bit)) {
+                unset($bits[$i]);
             }
         }
+
+        // *** Remove script file name from URL
+        $real_file = array_pop($bits);
+        $real_dir  = implode('/', $bits);
+
+        $virt_dir = '';
+        $get      = '';
+
+        // *** Store GET for later parsing
+        @list ($virt_dir, $get) = explode('?', $_SERVER['REQUEST_URI'], 2);
+
+        // *** Parse virtual directory
+        if (isset($this->_name) && isset($_GET[$this->_name])) {
+            $virt_dir = $_GET[$this->_name];
+        }
+
+        $bits = explode('/', $virt_dir);
 
         foreach ($bits as $i => $bit)
         {
@@ -111,57 +121,61 @@ class AeRequest extends AeObject
             }
         }
 
-        $script_name = implode('/', $bits);
+        $virt_dir = implode('/', $bits);
 
-        if (!empty($script_name)) {
-            $this->_directory = $script_name;
+        if (!isset($this->_name) || !isset($_GET[$this->_name])) {
+            // *** Remove real directory from the virtual part
+            $virt_dir = preg_replace('#^/?' . $real_dir . '/?#', '', $virt_dir, 1);
         }
 
-        $virtual = '';
-        $get     = '';
+        /*
+         * Since the class settings may be incorrect (although this case is
+         * not fully supported), only allow removing the script name from
+         * the URL, not require it to be there. In this case, if it is in
+         * the URL, it will be removed from the virtual part, but if it is
+         * not, it will not break the parsing
+         */
+        if ($this->_rewrite !== true) {
+            $count    = 0;
+            $virt_dir = preg_replace('#^/?(?:' . $real_file . '/?){1}#', '', $virt_dir, 1, $count);
+        }
 
-        @list ($virtual, $get) = explode('?', $_SERVER['REQUEST_URI'], 2);
-
-        // *** Parse virtual directory
-        if (!isset($this->_name) || !isset($_GET[$this->_name]))
+        if (!empty($virt_dir) || !isset($count) || $count != 1)
         {
-            $bits = explode('/', $virtual);
-
-            foreach ($bits as $i => $bit)
-            {
-                if (empty($bit)) {
-                    unset($bits[$i]);
-                }
+            if ($this->_rewrite !== true && isset($count) && $count == 1) {
+                $real_dir = empty($real_dir) ? $real_file : $real_dir . '/' . $real_file;
             }
 
-            $virtual = implode('/', $bits);
-            $virtual = preg_replace('#^/?' . $script_name . '/?#', '', $virtual);
+            $last      = strrpos($virt_dir, '/');
+            $virt_file = substr($virt_dir, $last > 0 ? $last + 1 : 0);
+            $virt_dir  = substr($virt_dir, 0, $last);
         } else {
-            $virtual = $_GET[$this->_name];
+            $virt_file = $real_file;
         }
 
         // *** Parse file
-        $last = strrpos($virtual, '/');
-        $file = substr($virtual, $last > 0 ? $last + 1 : 0);
-
-        if (strpos($file, '.') > 0)
-        {
-            $vir = substr($virtual, 0, $last);
-            $ext = strtolower(substr(strrchr($file, '.'), 1));
-
-            if (empty($this->_extensions) || in_array($ext, $this->_extensions)) {
-                $this->_virtual = $vir;
-                $this->_file    = $file;
-            } else {
-                $this->_virtual = $virtual;
-            }
-        } else {
-            $this->_virtual = $virtual;
+        if (!empty($virt_file) && strpos($virt_file, '.') > 0) {
+            $ext = strtolower(substr(strrchr($virt_file, '.'), 1));
         }
 
+        if (!isset($ext) || (!empty($this->_extensions) && !in_array($ext, $this->_extensions))) {
+            $virt_dir  = empty($virt_dir) ? $virt_file : $virt_dir . '/' . $virt_file;
+            $virt_file = null;
+        }
+
+        $this->_directory = empty($real_dir) ? null : $real_dir;
+        $this->_virtual   = empty($virt_dir) ? null : $virt_dir;
+        $this->_file      = $virt_file;
+
         // *** Parse get
-        // TODO: add GET parsing (including cleanup)
-        // TODO: after GET is parsed, add new parameters to GET and REQUEST
+        if ($get !== null) {
+            $this->_get = $get;
+
+            // *** This overwrites the whole GET array, leaving only new params
+            parse_str($get, $_GET);
+
+            // TODO: also assign new vars to REQUEST properly
+        }
 
         return $this;
     }
